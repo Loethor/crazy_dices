d908529080b344a53246f00ba22e8e41f1c0abc7
loethor <lrb2392@gmail.com>
2026-01-24 18:26:42 +0100

    Updated dice script and folder structure

--- dice/dice.gd
+++ dice/dice.gd
@@ -1,23 +1,24 @@
+class_name Dice
 extends Area2D
-class_name D6
+
+signal rolled(rolled_value:int, gold_value:int)
+
+@export var dice_type: Types.DICE_TYPE
+
+var dice_stats: DiceStats
+var number_of_faces: int
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-const SIX_FACES = 6
-var dice_stats:DiceStats
-var number_of_faces:int = 0
-
-signal rolled(rolled_value:int, gold_value:int)
-
 func _ready():
 	randomize()
-	dice_stats = DiceStats.new(Types.DICE_TYPE.D6)
+	dice_stats = DiceStats.new(dice_type)
 	number_of_faces = dice_stats.number_of_faces
 	_setup_sprite()
 
 func _setup_sprite() -> void:
-	sprite_2d.hframes = SIX_FACES
+	sprite_2d.hframes = number_of_faces
 	sprite_2d.vframes = 1
 	sprite_2d.frame = 0
 
--- main/game.gd
+++ main/game.gd
@@ -1,24 +1,32 @@
 extends Node2D
 
-const D_6 = preload("uid://p6b6xno8tuse")
+const D_6 := preload("uid://p6b6xno8tuse")
+const D_4 := preload("uid://uknn3h2no3y4")
+
 @onready var dice_positions: Node2D = $DicePositions
 @onready var gold_label: Label = $CanvasLayer/GoldLabel
 @onready var dices: Node2D = $Dices
+@onready var debug: VBoxContainer = $CanvasLayer/Debug
 
 var current_position: int = 0
 var player_gold:int = 0
 
+@export var is_debug_enabled:bool = true
+
 func _ready() -> void:
+	debug.visible = is_debug_enabled
 	_spawn_dice()
 	_update_gold_label(player_gold)
 
 
 func _spawn_dice():
 	if current_position < 6:
-		var d6: D6 = D_6.instantiate()
-		d6.position = get_current_position()
-		d6.rolled.connect(_update_player_gold)
-		dices.add_child(d6)
+		var dice_scenes:Array[PackedScene] = [D_4,D_6]
+		var random_dice = dice_scenes.pick_random()
+		var dice: Dice = random_dice.instantiate()
+		dice.position = get_current_position()
+		dice.rolled.connect(_update_player_gold)
+		dices.add_child(dice)
 		current_position += 1
 
 func _remove_dice():
